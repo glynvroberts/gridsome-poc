9fcd4468adfa2d7337f53780463db2371ce7069d
GlynRoberts <glynroberts@neemconsulting.com>
2020-03-25 08:12:45 +0000

    added netl

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,35 +6,5 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
-
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'posts/**/*.md',
-        typeName: 'Post',
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `static/admin`
-      }
-    },
-  ]
+  plugins: []
 }
